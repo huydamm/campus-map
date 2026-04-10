81faf9a561086f976d96208ee8ebe61318a08fab
Huy <huydamm77@email.com>
2026-04-09 21:37:59 -0400

    add vercel to CORS

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://campus-map-frontend.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
